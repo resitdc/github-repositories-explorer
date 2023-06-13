4faf00ab7e2c554cea88bead84b01a3dd1fe45fb
Restu Dwi Cahyo <restu@resitdc.id>
2023-06-13 17:01:24 +0700

    Add input style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "components/atoms";
 const Error: React.FC = () => (
   <div>
     <div>
-      <input type="search" name="" />
+      <input type="search" className="form-input" placeholder="Test" />
 
       <Button type="submit" color="primary" isLoading>
         Search
