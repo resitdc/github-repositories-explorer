76626e0eb566c3f51ff01dbfd07fe332dc05dce3
Restu Dwi Cahyo <restu@resitdc.id>
2023-06-14 01:49:26 +0700

    Fix skeleton loading component

--- src/components/atoms/SkeletonLoading/index.tsx
+++ src/components/atoms/SkeletonLoading/index.tsx
@@ -7,32 +7,28 @@ interface SkeletonLoadingProps {
   radius?: string;
 }
 
-const SkeletonLoading: React.FC = (
-  {
-    className,
-    width,
-    height,
-    radius
-  }: SkeletonLoadingProps) =>
-{
-  const classes: any[] = ["skeleton-loading"];
-  
+const SkeletonLoading: React.FC<SkeletonLoadingProps> = ({
+  className,
+  width,
+  height,
+  radius
+}) => {
+  const classes: string[] = ["skeleton-loading"];
+
   interface SkeletonStyle {
     width?: string;
     height?: string;
     borderRadius?: string;
-  };
+  }
 
   const skeletonStyle: SkeletonStyle = {};
 
-  classes.push(className);
+  if (className) classes.push(className);
   if (width) skeletonStyle.width = width;
   if (height) skeletonStyle.height = height;
   if (radius) skeletonStyle.borderRadius = radius;
 
-  return (
-    <div className={classes.join(" ")} style={skeletonStyle}></div>
-  );
+  return <div className={classes.join(" ")} style={skeletonStyle}></div>;
 };
 
 export default SkeletonLoading;
